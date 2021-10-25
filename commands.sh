bebf4fbd1b709f2fb5e938758941900c20c3d9c2
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-10-25 03:07:05 +0100

    Remove multi AZ resources

--- commands.sh
+++ commands.sh
@@ -1,2 +1,2 @@
 aws s3 cp ./cloudformation/ s3://dewei-jenkins/ --recursive;
-aws cloudformation update-stack --stack-name Jenkins --capabilities CAPABILITY_NAMED_IAM --template-body file://cloudformation/main.yml;
+aws cloudformation update-stack --stack-name Jenkins --capabilities CAPABILITY_NAMED_IAM --template-body file://cloudformation/main.yml --tags Key=service,Value=jenkins;
