52e8156c164db68e3a7f738997000b544fbe9e0a
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-10-28 01:15:48 +0100

    Add readme file

---
+++ README.md
@@ -0,0 +1,11 @@
+# Self-Hosted Jenkins Environment
+
+Available on [jenkins.dliu.com](http://jenkins.dliu.com)
+
+## Set up environment
+
+1. Set up AWS credentials and AWS configuration file
+1. Execute
+```
+bash -x commands.sh
+```
--- commands.sh
+++ commands.sh
@@ -1,3 +1,10 @@
+# Set up storage for Jenkins (EFS) and build artifacts (s3: dewei-artifacts)
 aws cloudformation create-stack --stack-name JenkinsEFS --template-body file://cloudformation/storage.yml --tags Key=service,Value=jenkins;
+
+# Upload the cloudformation templates onto the build artifact S3 bucket
 aws s3 cp ./cloudformation s3://dewei-artifacts/ --recursive;
-aws cloudformation update-stack --stack-name Jenkins --capabilities CAPABILITY_NAMED_IAM --template-url https://dewei-artifacts.s3.eu-west-2.amazonaws.com/jenkins/jenkins.yml --tags Key=service,Value=jenkins;
+
+# Create the Jenkins environment
+aws cloudformation create-stack --stack-name Jenkins --capabilities CAPABILITY_NAMED_IAM --template-url https://dewei-artifacts.s3.eu-west-2.amazonaws.com/jenkins/jenkins.yml --tags Key=service,Value=jenkins;
+
+# From now on, all jobs (building artifacts, deploy CDK/CloudFormation resources) can be done on Jenkins
