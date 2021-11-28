a470b4dd2a8bcfaa82e23fa7dd102140d5c98610
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-11-28 17:49:45 +0000

    Add destroy pipeline

--- commands.sh
+++ commands.sh
@@ -8,3 +8,6 @@ aws s3 cp ./cloudformation s3://dewei-artifacts/ --recursive;
 aws cloudformation update-stack --stack-name Jenkins --capabilities CAPABILITY_NAMED_IAM --template-url https://dewei-artifacts.s3.eu-west-2.amazonaws.com/jenkins.yml --tags Key=service,Value=jenkins;
 
 # From now on, all jobs (building artifacts, deploy CDK/CloudFormation resources) can be done on Jenkins
+
+# To delete stack
+aws cloudformation delete-stack --region eu-west-2 --stack-name Jenkins
