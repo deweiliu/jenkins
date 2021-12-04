d41da3a91022cccfcf446e6dad84ea76edd1d59e
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-12-04 00:07:43 +0000

    Update deployer permission

--- commands.sh
+++ commands.sh
@@ -5,7 +5,7 @@
 aws s3 cp ./cloudformation s3://dewei-artifacts/ --recursive;
 
 # Create the Jenkins environment
-aws cloudformation create-stack --stack-name Jenkins --capabilities CAPABILITY_NAMED_IAM --template-url https://dewei-artifacts.s3.eu-west-2.amazonaws.com/jenkins.yml --tags Key=service,Value=jenkins;
+aws cloudformation update-stack --stack-name Jenkins --capabilities CAPABILITY_NAMED_IAM --template-url https://dewei-artifacts.s3.eu-west-2.amazonaws.com/jenkins.yml --tags Key=service,Value=jenkins;
 
 # From now on, all jobs (building artifacts, deploy CDK/CloudFormation resources) can be done on Jenkins
 
