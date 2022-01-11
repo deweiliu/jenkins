1bb5b7b4ba57865fc97e64d51d8dc2a5d951261a
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2022-01-11 18:51:55 +0000

    Add jenkins agent setup script

---
+++ src/ubuntu-jenkins-agent-setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt update -y;
+sudo apt upgrade -y;
+
+# Jenkins agent requires Java
+sudo apt install default-jre -y; 
+
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt update -y;
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y;
