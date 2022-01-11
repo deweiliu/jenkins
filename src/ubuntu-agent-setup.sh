08c2f937311e72f01f6fa4a28014c328723dc946
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2022-01-11 18:54:55 +0000

    update jenkins agent setup script

--- src/ubuntu-agent-setup.sh
+++ src/ubuntu-agent-setup.sh
@@ -1,3 +1,4 @@
+# Init script upon connection
 #!/bin/sh
 
 sudo apt update -y;
@@ -11,3 +12,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 sudo apt update -y;
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y;
+
+# User data
+#!/bin/bash
+sudo groupadd docker;
+sudo usermod -aG docker ubuntu;
