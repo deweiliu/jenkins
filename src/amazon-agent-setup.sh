44907097d5801017263eacaeb975d9382709f5cf
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2022-01-12 19:13:14 +0000

    Update memory

--- src/amazon-agent-setup.sh
+++ src/amazon-agent-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt update -y;
-sudo apt upgrade -y;
-sudo apt install default-jre -y;
-sudo apt install docker.io docker-compose -y;
+sudo yum -y update --security;
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash - ;
+sudo yum install -y git nodejs gcc-c++ make; # Install essential packages
+sudo yum install -y java-1.8.0-openjdk; # Install Java to execute Jenkins agent
